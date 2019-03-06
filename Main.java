4dd2a9e121a16318aa1c2a340df5ddb336ebf6a5
Albina Alam <albinaalam@gmail.com>
2019-03-07 00:49:37 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import service.*;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 option o= new option();
+		 
+		 o.welcome();
+	    
+	    	
+	     
+	}
+
+}
